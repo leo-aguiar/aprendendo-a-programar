38fb50f7e81ff62320bcee9bbfb69e74b69d41ef
Leonardo de Aguiar <leo.nardo.aguiar@hotmail.com>
2022-08-25 19:19:33 -0300

    primeiros códigos da prática 1

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,6 @@
+//Declaração de 5 variáveis com seus, respectivos, valores
+let nome = "Leonardo"
+let email = "leo.nardo.aguiar@hotmail.com"
+let profissao = "Engenheiro de Software"
+let idade = 25
+let local = "Tokyo"
