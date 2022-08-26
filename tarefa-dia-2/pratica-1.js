acae20a76d7652466d73a3e9fe477a30fdd7ca1b
Leonardo de Aguiar <leo.nardo.aguiar@hotmail.com>
2022-08-25 22:20:05 -0300

    atualização do código

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -2,5 +2,11 @@
 let nome = "Leonardo"
 let email = "leo.nardo.aguiar@hotmail.com"
 let profissao = "Engenheiro de Software"
-let idade = 25
+let idade = "25"
 let local = "Tokyo"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(local)
