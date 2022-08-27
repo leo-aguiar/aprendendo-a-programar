5105a932149a33a1b9216a87e61013135cc810b7
Leonardo de Aguiar <leo.nardo.aguiar@hotmail.com>
2022-08-26 23:51:08 -0300

    código da pratica-1

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,13 @@
+// Considere que o dado de entrada para executar o código é o número n. Ou seja, uma variável nque vai querer 
+// o valor do número final que exibir;
+// Neste problema, você deve usar o comando while;
+// Exiba todos os números de 1 a n. Por exemplo, se o valor de n for 9, você deverá exibir o seguinte resultado
+// na tela com console.log();
+
+let n = 9
+let contador = 1
+
+while (contador <= n) {
+    console.log(contador)
+    contador ++
+}
